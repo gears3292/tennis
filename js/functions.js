ca726062d03de7b7456d46253a5d967d57d2f029
gears3292 <gears3292@gmail.com>
2016-05-03 17:10:22 -0600

    Sphere ball
    Turned square ball into sphere, removed(commented-out) y-axis ball so
    ball only goes left and right, and stored the player’s and opponent’s
    paddle into variables.

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,12 @@
 var canvas;
 var canvasContext;
 var ballX = 25;
-var ballSpeedX = 5;
-var ballY = 345;
-var ballSpeedY = 5;
+var ballSpeedX = 5; // change back to 5 when done
+//var ballY = 345;
+//var ballSpeedY = 5;
+var playerOne;
+var cpuOpponent;
+var counter = 0;
 
 window.onload = function() {
   //console.log('test');
@@ -19,21 +22,30 @@ window.onload = function() {
 
 function moveEverything() {
   ballX = ballX + ballSpeedX;
-  ballY = ballY + ballSpeedY;
+  //ballY = ballY + ballSpeedY;
   if (ballX > canvas.width || ballX < 0) {
     ballSpeedX = -ballSpeedX;
+    // console.log(counter += 1);
   }
-  if (ballY > canvas.height || ballY < 0) {
-    ballSpeedY = -ballSpeedY;
+  // else if (ballY > canvas.height || ballY < 0) {
+  //   ballSpeedY = -ballSpeedY;
+  // }
+
+  if (ballX > playerOne) {
+    ballSpeedX = -ballSpeedX;
   }
   //ballY = ballY + 3;
 }
 
 function drawEverything() {
   colorRect(0,0,canvas.width,canvas.height, '#101010'); // BG
-  colorRect(10,300, 10,100, '#22ddee'); // player's paddle
-  colorRect(880,300, 10,100, '#ff2233'); // opponent's paddle
-  colorRect(ballX,ballY, 10,10, '#dddddd'); // ball
+  playerOne = colorRect(10,300, 10,100, '#22ddee'); // player's paddle
+  cpuOpponent = colorRect(880,300, 10,100, '#ff2233'); // opponent's paddle
+  //colorRect(ballX,345, 10,10, '#dddddd'); // ball
+  canvasContext.fillStyle = '#eeeeee';
+  canvasContext.beginPath();
+  canvasContext.arc(ballX,345, 7, 0, Math.PI * 2, true);
+  canvasContext.fill();
 }
 
 function colorRect(leftX,topY, width,height, drawColor) {
